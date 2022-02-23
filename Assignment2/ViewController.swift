759f4cd4d8f7c6cfd74ddf3f723cd4c93e267cf5
irsupera <97858090+irsupera@users.noreply.github.com>
2022-02-23 00:45:21 -0500

    Added functionality
    Added functionality to compute equation from screen/label

--- Assignment2/ViewController.swift
+++ Assignment2/ViewController.swift
@@ -9,6 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Input: UILabel!
+    
+    @IBAction func One() {
+        Input.text = Input.text! + "1"
+    }
+    @IBAction func Two() {
+        Input.text = Input.text! + "2"
+    }
+    @IBAction func Three() {
+        Input.text = Input.text! + "3"
+    }
+    @IBAction func Four() {
+        Input.text = Input.text! + "4"
+    }
+    @IBAction func Five() {
+        Input.text = Input.text! + "5"
+    }
+    @IBAction func Six() {
+        Input.text = Input.text! + "6"
+    }
+    @IBAction func Seven() {
+        Input.text = Input.text! + "7"
+    }
+    @IBAction func Eight() {
+        Input.text = Input.text! + "8"
+    }
+    @IBAction func Nine() {
+        Input.text = Input.text! + "8"
+    }
+    @IBAction func Add() {
+        Input.text = Input.text! + "+"
+    }
+    @IBAction func Subtract() {
+        Input.text = Input.text! + "-"
+    }
+    @IBAction func Multiply() {
+        Input.text = Input.text! + "x"
+    }
+    @IBAction func Divide() {
+        Input.text = Input.text! + "/"
+    }
+    @IBAction func Mod() {
+        Input.text = Input.text! + "%"
+    }
+    @IBAction func OPar() {
+        Input.text = Input.text! + "("
+    }
+    @IBAction func CPar() {
+        Input.text = Input.text! + ")"
+    }
+    @IBAction func Equal() {
+        let expression = NSExpression(format:Input.text!)
+        let value = expression.expressionValue(with: nil, context: nil) as? Int
+        Input.text = value?.description
+    }
+    @IBAction func Clear() {
+        Input.text = " "
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
